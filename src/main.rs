e46c331f20166985e620f2433480589721748dd1
Santhanagopalan <sgopalan58@gmail.com>
2022-09-07 17:47:37 -0500

    Resizing implemented

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 mod server_thread_handler;
 mod tcp_helper;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 use std::thread;
 use std::net::TcpListener;
 
 fn main() {
     // Create a hashtable
-    let capacity = 10;
-    let mut locked_striped_hashtable: Vec<Arc<Mutex<Vec<(i32, i32)>>>> = Vec::new();
-    for _ in 0..capacity {
-        locked_striped_hashtable.push(Arc::new(Mutex::new(Vec::new())));
+    let capacity = 1;
+    let locked_striped_hashtable: Arc<RwLock<Vec<Mutex<Vec<(i32, i32)>>>>> = Arc::new(RwLock::new(Vec::new()));
+    {
+        let mut buckets = locked_striped_hashtable.write().unwrap();
+        for _ in 0..capacity {
+            buckets.push(Mutex::new(Vec::new()));
+        }
     }
     // Get the address and open the port
     let address = "0.0.0.0:7878";
     let listener: TcpListener = TcpListener::bind(address).unwrap();
     for stream in listener.incoming() {
-        let mut arc_striped_hashtable: Vec<Arc<Mutex<Vec<(i32, i32)>>>> = Vec::new();
-        for i in 0..capacity {
-            arc_striped_hashtable.push(Arc::clone(&locked_striped_hashtable[i]));
-        }
+        let thread_specific_hashtable = Arc::clone(&locked_striped_hashtable);
         let stream = stream.unwrap();
-        let t = thread::spawn(move|| {
-            server_thread_handler::process(stream, arc_striped_hashtable);
+        thread::spawn(move|| {
+            server_thread_handler::process(stream, thread_specific_hashtable);
         });
     }
 }
--- src/server_thread_handler.rs
+++ src/server_thread_handler.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
@@ -8,9 +8,10 @@ fn convert_string_to_int(string: String) -> i32{
     return string.parse::<i32>().unwrap();
 }
 
-fn get(thread_locked_table: &Vec<Arc<Mutex<Vec<(i32, i32)>>>>, key: i32) -> Result<i32, i32> {
-    let index = key as usize % thread_locked_table.len();
-    let bucket = thread_locked_table[index].lock().unwrap();
+fn get(thread_locked_table: &Arc<RwLock<Vec<Mutex<Vec<(i32, i32)>>>>>, key: i32) -> Result<i32, i32> {
+    let buckets = thread_locked_table.read().unwrap();
+    let index = key as usize % buckets.len();
+    let bucket = buckets[index].lock().unwrap();
     thread::sleep(Duration::from_millis(400));
     let mut found = false;
     let mut value = -1;
@@ -30,9 +31,45 @@ fn get(thread_locked_table: &Vec<Arc<Mutex<Vec<(i32, i32)>>>>, key: i32) -> Resu
     }
 }
 
-fn put(thread_locked_table: &mut Vec<Arc<Mutex<Vec<(i32, i32)>>>>, key: i32, value: i32) -> Result<i32, i32> {
-    let index = key as usize % thread_locked_table.len();
-    let mut bucket = thread_locked_table[index].lock().unwrap();
+fn resize(thread_locked_table: &Arc<RwLock<Vec<Mutex<Vec<(i32, i32)>>>>>) {
+    let mut old_buckets = thread_locked_table.write().unwrap();
+    let a_bucket = old_buckets[0].lock().unwrap();
+    
+    if a_bucket.len() >= old_buckets.len(){
+        println!("RESIZING");
+        drop(a_bucket);
+        let mut buckets:Vec<Mutex<Vec<(i32, i32)>>> = Vec::new();
+        let old_capacity = old_buckets.len();
+        let new_capacity = old_buckets.len() * 2;
+        for _ in 0..new_capacity {
+            buckets.push(Mutex::new(Vec::new()));
+        }
+        for i in 0..old_capacity {
+            let old_bucket = old_buckets[i].lock().unwrap();
+            for i in 0..old_bucket.len() {
+                let new_index = old_bucket[i].0 as usize % new_capacity;
+                let mut new_bucket = buckets[new_index as usize].lock().unwrap();
+                new_bucket.push((old_bucket[i].0, old_bucket[i].1));
+            }
+        }
+        *old_buckets = buckets;
+    }
+    else {
+        return;
+    }
+
+}
+
+fn put(thread_locked_table: &Arc<RwLock<Vec<Mutex<Vec<(i32, i32)>>>>>, key: i32, value: i32) -> Result<i32, i32> {
+    let buckets = thread_locked_table.read().unwrap();
+    let index = key as usize % buckets.len();
+    let mut bucket = buckets[index].lock().unwrap();
+    if bucket.len() >= buckets.len() {
+        drop(bucket);
+        drop(buckets);
+        resize(&thread_locked_table);
+        return put(thread_locked_table, key, value);
+    }
     thread::sleep(Duration::from_millis(400));
     let mut found = false;
     for i in 0..bucket.len() {
@@ -48,14 +85,12 @@ fn put(thread_locked_table: &mut Vec<Arc<Mutex<Vec<(i32, i32)>>>>, key: i32, val
     return Ok(0);
 }
 
-pub fn process(mut stream: TcpStream, mut thread_locked_table: Vec<Arc<Mutex<Vec<(i32, i32)>>>>){
+pub fn process(mut stream: TcpStream, thread_locked_table: Arc<RwLock<Vec<Mutex<Vec<(i32, i32)>>>>>){
     
     let command_str = tcp_helper::read_command(&mut stream);
     let command_units = command_str.split_whitespace().collect::<Vec<_>>();
-    
     if command_units.len() < 2 {
         tcp_helper::write_string(&mut stream, "0\n".to_owned());
-        
         let error_value = "Server: Enter the command correctly".to_owned();
         tcp_helper::write_string(&mut stream, error_value);
         return; 
@@ -63,7 +98,6 @@ pub fn process(mut stream: TcpStream, mut thread_locked_table: Vec<Arc<Mutex<Vec
 
     let operation: &str = command_units[0];
     let key: i32 = convert_string_to_int(command_units[1].to_owned());
-    // println!("EXECUTING {}\n", command_str);
     if operation.eq("GET") {
         let error_code = match get(&thread_locked_table, key) {
             Ok(_) => "0\n".to_owned(),
@@ -80,7 +114,7 @@ pub fn process(mut stream: TcpStream, mut thread_locked_table: Vec<Arc<Mutex<Vec
 
     else if operation.eq("PUT"){
         let value = convert_string_to_int(command_units[2].to_owned());
-        let error_code = match put(&mut thread_locked_table, key, value){
+        let error_code = match put(&thread_locked_table, key, value){
             Ok(_) => "0\n".to_owned(),
             Err(_) => "1\n".to_owned(),
         };
@@ -90,7 +124,6 @@ pub fn process(mut stream: TcpStream, mut thread_locked_table: Vec<Arc<Mutex<Vec
 
     else {
         tcp_helper::write_string(&mut stream, "1\n".to_owned());
-        
         let error_code = "Server: FAILED - Wrong command\n".to_owned();
         tcp_helper::write_string(&mut stream, error_code); 
     }
