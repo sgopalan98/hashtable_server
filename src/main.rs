b35dcff21d2419fe030aec5666b3c3121e7e5093
Santhanagopalan <sgopalan58@gmail.com>
2022-12-21 11:23:53 +0530

    Added capability to pass capacity and no of threads

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,49 @@ use std::net::TcpListener;
 use dashmap::DashMap;
 use thread_handlers::dashmap_thread_handler;
 
+fn convert_string_to_int(string: String) -> usize{
+    let string = string.trim();
+    return string.parse::<usize>().unwrap();
+}
 
 fn main() {
-    // Create a hashtable
-    let capacity = 1000;
-    let locked_dashmap: Arc<DashMap<u128, u128>> = Arc::new(DashMap::with_capacity(capacity));
-
-    // Get the address and open the port
+    // Start server 
     let address = "0.0.0.0:7879";
-    let listener: TcpListener = TcpListener::bind(address).unwrap();
+    let listener : TcpListener = TcpListener::bind(address).unwrap();
 
-    for stream in listener.incoming() {
-        let thread_specific_hashtable = Arc::clone(&locked_dashmap);
-        let stream = stream.unwrap();
-        thread::spawn(move|| {
-            dashmap_thread_handler::process(stream, thread_specific_hashtable);
-        });
+    // First connection should get capacity and no of threads
+    loop {
+        let mut capacity = 0;
+        let mut no_of_threads = 0;
+
+        for stream in listener.incoming().take(1) {
+            let mut stream = stream.unwrap();
+            let command = tcp_helper::read_command(&mut stream);
+            let command_units = command.split_whitespace().collect::<Vec<_>>();
+            let capacity_command = command_units[0].to_owned();
+            let no_of_threads_command = command_units[1].to_owned();
+            println!("{} {}\n", capacity_command, no_of_threads_command);
+            capacity = convert_string_to_int(capacity_command);
+            no_of_threads = convert_string_to_int(no_of_threads_command);
+        }
+        // Create a hashtable with that capacity
+        let dashmap = DashMap::with_capacity(capacity);
+        let locked_dashmap: Arc<DashMap<u128, u128>> = Arc::new(dashmap);
+
+        // Create worker threads - #said no of threads
+        let mut threads = vec![];
+        for stream in listener.incoming().take(no_of_threads) {
+            let thread_specific_hashtable = Arc::clone(&locked_dashmap);
+            let stream = stream.unwrap();
+            threads.push(thread::spawn(move|| {
+                dashmap_thread_handler::process(stream, thread_specific_hashtable);
+            }));
+        }
+
+        // Wait for the threads to finish
+        for thread in threads {
+            thread.join().unwrap();
+        }
     }
 }
 
